45b3b8111f5863071f5cf025064cd47e36e4561d
eslamabosalem <eslamabosalem22@gmail.com>
2025-05-13 02:46:52 +0300

    Done

--- src/Component/Geography/Geo.jsx
+++ src/Component/Geography/Geo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import Header from '../Header.jsx/Header'
+
 import { Box } from '@mui/material'
+
+import Header from '../Header/Header';
 import Geography from './Geography';
 
 
